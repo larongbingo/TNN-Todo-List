624a2586c9ff15a215e2d099e8c5c10d4b8df139
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-09-15 14:14:55 +0800

    Added the Todo Model

---
+++ TodoList/TodoList/Models.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Realms;
+
+namespace TodoList
+{
+    /// <summary>
+    /// Represents all of the entire todo list
+    /// </summary>
+    class Todo : RealmObject
+    {
+        public string TodoMessage = "";
+        public bool isTodoDone = false;
+    }
+}
